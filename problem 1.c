cd6516dbad83835e1919d5ef2c56de28ab0bb7a1
Priyam Sharma <priyamsharmaop@gmail.com>
2026-08-10 22:55:29 +0530

    Initial commit

---
+++ problem 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+    int a,b,sum;
+    printf("Enter the value of a and b: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("The sum is: %d", sum);
+
+    return 0;
+}
