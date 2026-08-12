ab5fe23b8c0b207c6d570773fbd78b265e52e4ad
Priyam Sharma <priyamsharmaop@gmail.com>
2026-08-12 23:11:03 +0530

    day3_problem1.c

---
+++ day2_problem2.c
@@ -0,0 +1,19 @@
+//write a c program to calculate the area and circumference of a circle given its radius.
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+
+    float pi = 3.14;
+    printf( "Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area=pi * radius * radius;
+    circumference = 2 * pi * radius;
+
+    
+    printf("Area of the circle: %.2f\n", area);
+    printf("Circumference of the circle: %.2f\n", circumference);
+
+    return 0;
+}
---
+++ day3_problem1.c
@@ -0,0 +1,17 @@
+//write a program to convert temperature from Celsius to Fahrenheit.
+#include <stdio.h>
+
+int main() {
+
+    float c,f;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f",&c);
+
+    f=(c*9/5)+32;
+    printf("temperature in fahrenheit = %.2f",
+    )
+    
+
+    return 0;
+    
