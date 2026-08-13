77955b4280f477f6c16943fd9d0ee0079fe58729
Priyam Sharma <priyamsharmaop@gmail.com>
2026-08-13 22:40:11 +0530

    day4_problem1.c

---
+++ day4_problem1.c
@@ -0,0 +1,20 @@
+//write a program to swap two numbers without using a third variable
+
+#include <stdio.h>
+
+int main ()
+{
+    int a, b;
+      
+    printf("enter two numbers : ");
+    scanf("%d %d", &a, &b); 
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("after swap: %d %d" , a, b);
+
+return 0;
+}
+
