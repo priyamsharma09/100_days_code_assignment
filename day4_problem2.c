e34d94deeaba4f236362d53feaeb6a5594693726
Priyam Sharma <priyamsharmaop@gmail.com>
2026-08-13 23:27:28 +0530

    day4_problem2.c

---
+++ day4_problem2.c
@@ -0,0 +1,19 @@
+//write a program to find and display the sum of the first n natural numbers.
+
+#include <stdio.h>
+
+int main ()
+{
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("sum of first %d natural numbers is %d", n, sum);
+    return 0;
+    
+}
