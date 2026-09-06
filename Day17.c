5fd64add72d9f6d3e5fd7b139c85618b2f1e4417
priyamsharma09 <priyamsharmaop@gmail.com>
2026-09-06 22:20:31 +0530

    Add files via upload

---
+++ Day17.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+
+int main() {
+    int x=3;
+    x+=5;
+    printf("%d",x);
+
+    printf("after inc %d",x++);
+
+    printf("after dec %d",--x);
+     return 0;
+
+
+
+    }
+
+
+
+
+
+
+    return 0;
+}
