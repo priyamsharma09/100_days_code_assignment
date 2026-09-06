5b615cd507c1f21c7e8c70a82ed2711beb9e9654
priyamsharma09 <priyamsharmaop@gmail.com>
2026-09-06 22:18:17 +0530

    Add files via upload

---
+++ Day14.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int main() {
+    int a;
+    long product = 1; 
+    int even = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &a);
+
+    for (int i = 2; i <= a; i += 2) {
+        product *= i;
+        even = 1;
+    }
+
+    if (even && a >= 2) {
+        printf("The product of even numbers from 1 to %d is: %lld\n", a, product);
+    } else {
+        printf("There are no even numbers in the range 1 to %d.\n", a);
+    }
+
+
+
+    return 0;
+}
