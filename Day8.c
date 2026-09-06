445d32ad5474bb8966fadde90b76fd2e41943882
priyamsharma09 <priyamsharmaop@gmail.com>
2026-09-06 22:10:11 +0530

    Add files via upload

---
+++ Day8.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("A:");
+    scanf("%d",&a);
+    printf("B:");
+    scanf("%d",&b);
+    printf("C:");
+    scanf("%d",&c);
+
+    if( a>=b && a>=c){
+        printf("A IS GREATER.\n");
+    }
+    else if( b>=a && b>=c){
+        printf("B IS GREATER.\n");
+    }
+    else{
+        printf("C IS GREATER.\n");
+    }
+    return 0;
+}
