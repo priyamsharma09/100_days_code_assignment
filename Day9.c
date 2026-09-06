a8591c317987ec2400ec89b1f778a0034e3dde55
priyamsharma09 <priyamsharmaop@gmail.com>
2026-09-06 22:15:05 +0530

    Add files via upload

---
+++ Day9.c
@@ -0,0 +1,54 @@
+
+#include<stdio.h>
+int main(){
+    int marks;
+
+
+    printf("ENTER THE MARKS:");
+    scanf("%d",&marks);
+
+
+
+
+    if(marks >=90){
+    printf("GRADE A");
+
+
+
+
+
+
+
+
+
+
+
+    }
+    else if(marks >= 80){
+        printf("GRADE B");
+    }
+    else if(marks >=70){
+        printf("GRADE C");
+    }
+    else if(marks >=60){
+        printf("GRADE D");
+    }
+    else{
+        printf("GRADE F");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    return 0;
+}
